5ae742d600ef6d16264e149734bfabf95575fde2
wjjxqx <37838883+wjjxqx@users.noreply.github.com>
2021-04-06 23:41:36 +0800

    Update koolproxyupdate.sh

--- koolproxyupdate.sh
+++ koolproxyupdate.sh
@@ -1,14 +1,4 @@
-#wget 'https://kprule.com/kp.dat' -O files/usr/share/koolproxy/data/rules/kp.dat
-#wget 'https://kprule.com/koolproxy.txt' -O files/usr/share/koolproxy/data/rules/koolproxy.txt
-#wget 'https://kprule.com/daily.txt' -O files/usr/share/koolproxy/data/rules/daily.txt
-#wget 'https://kprule.com/user.txt' -O files/usr/share/koolproxy/data/user.txt
-#wget 'https://koolproxy.com/downloads/arm' -O files/bin/arm
-#wget 'https://koolproxy.com/downloads/i386' -O files/bin/i386
-#wget 'https://koolproxy.com/downloads/mips' -O files/bin/mips
-#wget 'https://koolproxy.com/downloads/mipsel' -O files/bin/mipsel
-#wget 'https://koolproxy.com/downloads/x86_64' -O files/bin/x86_64
-#chmod +x files/bin/*
-
-wget https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt -O- | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > files/usr/share/koolproxy/dnsmasq.adblock
-sed -i '/youku/d' files/usr/share/koolproxy/dnsmasq.adblock
-sed -i '/[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}/d' files/usr/share/koolproxy/dnsmasq.adblock
+#!/bin/sh
+# 本程序由恩山-xqxwjj-改版，请勿商用,欢迎捐赠
+wget -O /usr/share/koolproxy/data/rules/kp.dat https://cdn.jsdelivr.net/gh/houzi-/CDN@master/kp.dat --no-check-certificate && wget -O /usr/share/koolproxy/data/rules/daily.txt https://cdn.jsdelivr.net/gh/houzi-/CDN@master/daily.txt --no-check-certificate && wget -O /usr/share/koolproxy/data/rules/koolproxy.txt https://cdn.jsdelivr.net/gh/houzi-/CDN@master/koolproxy.txt --no-check-certificate && /etc/init.d/koolproxy restart
+#wget -O /usr/share/koolproxy/data/rules/kp.dat https://down.cmccw.xyz/kp.dat --no-check-certificate && wget -O /usr/share/koolproxy/data/rules/daily.txt https://down.cmccw.xyz/daily.txt --no-check-certificate && wget -O /usr/share/koolproxy/data/rules/koolproxy.txt https://down.cmccw.xyz/koolproxy.txt --no-check-certificate && wget -O /usr/share/koolproxy/data/rules/kpr_rule.txt https://kt-wong.coding.net/p/KPR/d/KPR/git/raw/master/kpr_rule.txt --no-check-certificate
